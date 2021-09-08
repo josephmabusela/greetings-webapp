f63b2effee8ea655c281c0ca12ddf9ea0d047614
josephmabusela <josephmabusela5@gmail.com>
2021-09-08 14:03:34 +0200

    counter route fixes

--- greeting.js
+++ greeting.js
@@ -23,39 +23,6 @@ module.exports =  function Greetings() {
         return greetMessage
     }
 
-    // function errorText() {
-    //     // prompt usser to enter a valid name with no numbers or characters
-    //     if (!nameText.value.match(/[a-zA-Z]/ig)) {
-    //         errorText.innerHTML = "Please enter valid name"
-    //         errorText.style.color = "red"
-
-    //         setTimeout(() => {
-    //             errorText.innerHTML = ""
-    //         }, 3000);
-    //     }
-
-    //     // prompt user to enter a name if they havent
-    //     if (nameText.value === "") {
-    //         errorText.innerHTML = "Enter a name";
-    //         errorText.style.color = "red"
-
-    //         setTimeout(() => {
-    //             errorText.innerHTML = ""
-    //         }, 3000);
-    //     }
-
-    //     // prompt user to select a language if they havent
-    //     if (!radioBtn) {
-    //         errorText.innerHTML = "Select a language";
-    //         errorText
-    //         .style.color = "red";
-
-    //         setTimeout(() => {
-    //             errorText.innerHTML = ""
-    //         }, 3000);
-    //     }
-    // }
-
     function recordGreetedNames(name) {
         // check if name exists
         if (namesList[name] === undefined) {
